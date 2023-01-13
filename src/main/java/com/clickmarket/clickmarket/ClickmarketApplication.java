777f55c15cc33c38dc28c5e8599a104aa8373719
nidal-2810 <nidal.as2810@gmail.com>
2023-01-13 21:52:03 +0200

    Initial commit

---
+++ src/main/java/com/clickmarket/clickmarket/ClickmarketApplication.java
@@ -0,0 +1,13 @@
+package com.clickmarket.clickmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clickmarket/clickmarket/ClickmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickmarket.clickmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
